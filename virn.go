ad562a68a8d87bff31e81a46125d48a81045b4fe
kawasima <kawasima1016@gmail.com>
2014-11-12 10:36:25 +0900

    I forgot to save the file...

--- virn.go
+++ virn.go
@@ -2,27 +2,46 @@ package main
 
 import (
 	"io/ioutil"
+	"log"
 	"os"
+	"os/signal"
+	"path/filepath"
 	"strings"
-	"unicode"
+	"syscall"
 	"unicode/utf8"
 )
 
 import goncurses "github.com/gbin/goncurses"
 
-func FindFile(filename string) string {
+func findFile(filename string) string {
 	for _, e := range os.Environ() {
 		pair := strings.Split(e, "=")
 		if pair[0] == "HOME" {
-			return pair[1] + "/.virn/" + filename
+			return filepath.Join(pair[1], "/.virn/", filename)
 		}
 	}
 	return filename
 }
 
+func renderInitialWindow(w *goncurses.Window) {
+	y, _ := w.MaxYX()
+	for i := 1; i < y-1; i++ {
+		w.MoveAddChar(i, 0, '~')
+	}
+	w.Move(0, 0)
+	w.Refresh()
+}
+
+func terminateWindow() {
+	log.Println("TERMINATE")
+	goncurses.CBreak(false)
+	goncurses.NewLines(false)
+	goncurses.End()
+}
+
 func main() {
 	filename := os.Args[len(os.Args)-1]
-	filepath := FindFile(filename)
+	filepath := findFile(filename)
 	bs, _ := ioutil.ReadFile(filepath)
 	s := string(bs[:])
 
@@ -30,7 +49,15 @@ func main() {
 	if err != nil {
 
 	}
-	defer goncurses.End()
+	defer terminateWindow()
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		terminateWindow()
+		os.Exit(1)
+	}()
+
 	goncurses.CBreak(true)
 	scr.ScrollOk(true)
 	scr.Keypad(true)
@@ -46,6 +73,7 @@ func main() {
 	status_win.ClearOk(true)
 	status_win.Printf("\"%s\" [New File]", filename)
 	status_win.Refresh()
+	renderInitialWindow(scr)
 
 	scr.GetChar()
 	status_win.Erase()
@@ -63,14 +91,16 @@ func main() {
 		rune, _ := utf8.DecodeRuneInString(chars[i])
 		if rune == '\n' {
 			if ch == '\n' {
+				scr.Print("\n")
+				scr.DelChar()
 				for true {
+					i++
+					if i >= len(chars) {
+						break
+					}
 					r, _ := utf8.DecodeRuneInString(chars[i])
-					if unicode.IsSpace(r) {
+					if r == '\t' || r == ' ' {
 						scr.Printf("%c", r)
-						i++
-						if i >= len(chars) {
-							break
-						}
 					} else {
 						break
 					}
@@ -91,5 +121,6 @@ func main() {
 	scr.Refresh()
 	scr.GetChar()
 	scr.GetChar()
-	goncurses.End()
+
+	ioutil.WriteFile(filename, []byte(s), 0644)
 }
